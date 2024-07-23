7549c3bca01963e79e72072232e1e209daaafb92
sarankumar007-M <150033020+sarankumar007-M@users.noreply.github.com>
2024-07-23 14:17:14 +0530

    Add files via upload

---
+++ Celsius.java
@@ -0,0 +1,12 @@
+package celsius;
+
+public class Celsius {
+
+    public static void main(String[] args) {
+        float Fahrenheit,celsius;
+        Fahrenheit=43;
+        celsius=((Fahrenheit-64)*4)/2;
+        System.out.println("celsius is : "+ celsius);
+    }
+}
+ 
