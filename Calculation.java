c10b4a78cdabfc34b8bb2849997f59870bcff0bf
sarankumar007-M <150033020+sarankumar007-M@users.noreply.github.com>
2024-07-23 14:15:17 +0530

    Add files via upload

---
+++ Calculation.java
@@ -0,0 +1,12 @@
+package calculation;
+public class Calculation {
+
+    public static void main(String[] args) {
+        int sum=0;
+        for(int j = 1;j<=45;j++){
+            sum=sum+j;
+        }
+        System.out.println("The sum of first 10 natural numbers is"+sum);
+    }
+    
+}
